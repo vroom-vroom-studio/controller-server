474f77c894925634327be0aab8cf4cd801b6ff02
Laurent Lubino <laurent.lubino@weareroam.com>
2020-03-06 16:30:41 +1300

    feature: ControllerManager structure start

--- src/ControllerManager.ts
+++ src/ControllerManager.ts
@@ -1,5 +1,41 @@
-export default class ControllerManager {
-  constructor() {
+import { Server, Socket } from 'socket.io'
+import uuid from 'uuid'
 
+export interface Controller {
+  id: string;
+  socketId: string;
+  data: any;
+}
+
+export interface Client {
+  id: string;
+  socketId: string;
+}
+
+export default class ControllerManager {
+  private controllers: Controller[];
+  private clients: Client[];
+
+  constructor(io: Server) {
+    this.controllers = []
+    this.clients = []
+    io.on('connection', this.manageSocket)
   }
+
+  private manageSocket = (socket: Socket) => {
+    // client events
+    socket.on('client-connection', this.onClientConnection(socket))
+    socket.on('client-disconnection', this.onClientDisconnection(socket))
+    // controller events
+    socket.on('controller-disconnection', this.onControllerDisconnection(socket))
+    socket.on('controller-connection', this.onControllerConnection(socket))
+    socket.on('controller-update', this.onControllerUpdate(socket))
+  }
+  // client management
+  private onClientConnection = (socket: Socket) => () => { }
+  private onClientDisconnection = (socket: Socket) => () => { }
+  // controller management
+  private onControllerConnection = (socket: Socket) => () => { }
+  private onControllerDisconnection = (socket: Socket) => () => { }
+  private onControllerUpdate = (socket: Socket) => () => { }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import ControllerManager from './ControllerManager';
+import http from 'http'
+import socketIO from 'socket.io'
+import ControllerManager from './ControllerManager'
 
-const cm = new ControllerManager();
+const PORT = 8080
+
+const server = http.createServer()
+server.listen(PORT)
+const io = socketIO(server);
+
+const cm = new ControllerManager(io)
 
 export default ControllerManager
